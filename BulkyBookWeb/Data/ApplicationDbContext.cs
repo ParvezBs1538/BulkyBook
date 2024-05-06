ba28c1c1f26293eb6c6cb40127d4da0ce88eb482
Masud Parvez <masud.parvez@brainstation.com>
2024-05-06 18:05:43 +0600

    Create category class

---
+++ BulkyBookWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using BulkyBookWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyBookWeb.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        //public ApplicationDbContext(DbContextOptions options) : base(options)
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Category> Categories { get; set; } // class-Name, Table-Name
+    }
+}
