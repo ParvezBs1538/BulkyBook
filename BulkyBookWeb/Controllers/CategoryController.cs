8d0c40190b2e12ed19f5792f69d6eede77e699b1
Masud Parvez <masud.parvez@brainstation.com>
2024-05-06 19:20:21 +0600

    Display Category list

---
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -0,0 +1,21 @@
+﻿using BulkyBookWeb.Data;
+using BulkyBookWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyBookWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext context;
+        public CategoryController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+        public IActionResult Index()
+        {
+            //var item = context.Categories.ToList();
+            IEnumerable<Category> item = context.Categories.ToList();
+            return View(item);
+        } 
+    }
+}
