20dd4321fb466e86ffe176d2c10dd6c2a00f9281
Masud Parvez <masud.parvez@brainstation.com>
2024-05-07 14:36:31 +0600

    Delete Section Implemented

--- BulkyBookWeb/Controllers/CategoryController.cs
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -41,5 +41,74 @@ namespace BulkyBookWeb.Controllers
 			}
 			return View(obj);
 		}
-	}
+
+		// Get
+		[HttpGet]
+		public IActionResult Edit(int? id)
+		{
+            if (id == null || id == 0)
+			{
+				return NotFound();
+			}
+			//var item = context.Categories.Single(x => x.Id == id); // if no element found return exception
+			//var item = context.Categories.SingleOrDefault(x => x.Id == id); // if no element found return empty (it throw exception if more than element found)
+			//var item = context.Categories.FirstOrDefault(x => x.Id == id); // if found more than one then return first element
+			var item = context.Categories.Find(id);
+			if (item == null)
+			{
+				return NotFound();
+			}
+            return View(item);
+		}
+		// post
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public IActionResult Edit(Category obj)
+		{
+			if (obj.Name == obj.DisplayOrder.ToString())
+			{
+				ModelState.AddModelError("name", "The DisplayOrder cannot exactly match the Name.");
+			}
+			if (ModelState.IsValid)
+			{
+				context.Categories.Update(obj);
+				context.SaveChanges();
+				TempData["success"] = "Category Updated successfully";
+				return RedirectToAction("Index");
+			}
+			return View(obj);
+		}
+
+        // Get
+        [HttpGet]
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var item = context.Categories.Find(id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+            return View(item);
+        }
+        // post
+        [HttpPost]
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePost(int? id)
+        {
+            var item = context.Categories.Find(id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+            context.Categories.Remove(item);
+            context.SaveChanges();
+            TempData["success"] = "Category Delete successfully";
+            return RedirectToAction("Index");
+        }
+    }
 }
