8dc2c38ad9e651ca8f1ba2348e381fbcaedac9ef
Masud Parvez <masud.parvez@brainstation.com>
2024-05-07 12:53:37 +0600

    Create Section implement

--- BulkyBookWeb/Controllers/CategoryController.cs
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -16,6 +16,30 @@ namespace BulkyBookWeb.Controllers
             //var item = context.Categories.ToList();
             IEnumerable<Category> item = context.Categories.ToList();  // IEnumerable-> represents a sequence of elements
             return View(item);
-        } 
-    }
+        }
+        // Get
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        // post
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+		public IActionResult Create(Category obj)
+		{
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The DisplayOrder cannot exactly match the Name.");
+            }
+            if (ModelState.IsValid)
+			{
+				context.Categories.Add(obj);
+				context.SaveChanges();
+				TempData["success"] = "Category created successfully";
+				return RedirectToAction("Index");
+			}
+			return View(obj);
+		}
+	}
 }
